1ccfeff48ec4076584195fffb35f737f02b5c4c4
lusgem <florian.lherbeil@yahoo.fr>
2019-04-21 15:33:37 +0200

    Final Commit

--- src/GenerateurInstance.java
+++ src/GenerateurInstance.java
@@ -8,16 +8,14 @@ public class GenerateurInstance {
      */
     public static Plateau generer() {
         Scanner in = new Scanner(System.in);
-        int hauteur,largeur, nbBat;
+        int valeur;
         Plateau plateau;
-        System.out.println("Entrez la hauteur de la map : ");
-        hauteur = in.nextInt();
-        System.out.println("Entrez la largeur : ");
-        largeur = in.nextInt();
-        plateau = new Plateau(hauteur,largeur);
-        System.out.println("Combien de batiments voulez vous générer : ");
-        nbBat = in.nextInt();
-        plateau.setNbBatiments(nbBat);
+        System.out.println("Entrez la hauteur de la map \n" +
+                "La largeur de la map sera égale à cette hauteur\n" +
+                "Le nombre de batiment sera aussi égal à cette valeur :");
+        valeur = in.nextInt();
+        plateau = new Plateau(valeur,valeur);
+        plateau.setNbBatiments(valeur);
         int min = 1, max = (int)(2 * Math.sqrt(plateau.getHauteur()));
         for(int i = 1; i <= plateau.getNbBatiments(); i++) {
             plateau.getBatiments().add(new Batiment((int)(min + (Math.random() * (max - min) + 1)),(int)(min + (Math.random() * (max - min) + 1)),i));
